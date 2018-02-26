c666183758cb721863569b5084ddacb08b01ba70
Ricky Miller <ricky.miller@gmail.com>
2018-02-26 17:16:52 -0500

    Make 06 is a bit more readable

--- 06-make-it-modular/m.js
+++ 06-make-it-modular/m.js
@@ -1,1 +1,3 @@
-module.exports=(a,b,c)=>require('fs').readdir(a,(_,f)=>_?c(_):c(null,f.filter(f=>RegExp('.'+b).test(f))))
+module.exports=(a,b,c)=>
+  require('fs').readdir(a,(_,f)=>_?c(_):c(null,f.filter(f=>RegExp('.'+b)
+  .test(f))))
