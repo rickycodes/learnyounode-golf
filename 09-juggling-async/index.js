eccea77dd342c436c1065be401dc722dd300dbf2
Ricky Miller <ricky.miller@gmail.com>
2018-02-26 09:41:35 -0500

    Simplify 09

--- 09-juggling-async/index.js
+++ 09-juggling-async/index.js
@@ -1,4 +1,4 @@
-c=[],j=0,m='map';process.argv.slice(2)[m]((u,i)=>{
+j=0,m='map';process.argv.slice(2)[m]((u,i,c)=>{
 c[i]='';require('http').get(u,r=>
   r.on('data',r=>c[i]+=r).on('end',_=>j++&&j==3&&c[m](r=>console.log(r)))
 )})
