6de0a63b92a6af85836fb664c5fc831280e1c5a9
Ricky Miller <ricky.miller@gmail.com>
2018-03-01 02:33:55 -0500

    bleh

--- generate_readmes.js
+++ generate_readmes.js
@@ -1,5 +1,38 @@
-// TODO: fix this up a bit so it isn't complete trash
 const fs = require('fs')
+
+// const { exercises } = require('/usr/local/lib/node_modules/learnyounode/i18n/en.json')
+// console.log(exercises)
+
+const titles = [
+  'HELLO WORLD',
+  'BABY STEPS',
+  'MY FIRST I/O!',
+  'MY FIRST ASYNC I/O!',
+  'FILTERED LS',
+  'MAKE IT MODULAR',
+  'HTTP CLIENT',
+  'HTTP COLLECT',
+  'JUGGLING ASYNC',
+  'TIME SERVER',
+  'HTTP FILE SERVER',
+  'HTTP UPPERCASERER',
+  'HTTP JSON API SERVER'
+]
+
+console.log(titles)
+
+const ex = '/usr/local/lib/node_modules/learnyounode/exercises/'
+fs.readdir(ex, (error, files) => {
+  files.map((file, index) => {
+    let num = ++index
+    if (num < 10) {
+      num = `0${num}`
+    }
+    console.log(num+file)
+  })
+}) && process.exit()
+
+// TODO: fix this up a bit so it isn't complete trash
 fs.readdir('.', (error, files) => {
   const folders = files.filter(file => /\d\d-\D/.test(file))
   folders.map(folder => {
