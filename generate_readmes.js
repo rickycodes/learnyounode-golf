300dd6fbc9c7cc459ee6181b809683ac5dbbee67
Ricky Miller <ricky.miller@gmail.com>
2018-03-01 04:49:39 -0500

    hope this works!

--- generate_readmes.js
+++ generate_readmes.js
@@ -1,53 +1,53 @@
 const fs = require('fs')
+const util = require('util')
 
-// const { exercises } = require('/usr/local/lib/node_modules/learnyounode/i18n/en.json')
-// console.log(exercises)
+// location of globally instealled learnyounode
+const ex = '/usr/local/lib/node_modules/learnyounode/exercises/'
 
-const titles = [
-  'HELLO WORLD',
-  'BABY STEPS',
-  'MY FIRST I/O!',
-  'MY FIRST ASYNC I/O!',
-  'FILTERED LS',
-  'MAKE IT MODULAR',
-  'HTTP CLIENT',
-  'HTTP COLLECT',
-  'JUGGLING ASYNC',
-  'TIME SERVER',
-  'HTTP FILE SERVER',
-  'HTTP UPPERCASERER',
-  'HTTP JSON API SERVER'
+const meta = [
+  { path: 'hello_world', name: 'HELLO WORLD' },
+  { path: 'baby_steps', name: 'BABY STEPS' },
+  { path: 'my_first_io', name: 'MY FIRST I/O!' },
+  { path: 'my_first_async_io', name: 'MY FIRST ASYNC I/O!' },
+  { path: 'filtered_ls', name: 'FILTERED LS' },
+  { path: 'make_it_modular', name: 'MAKE IT MODULAR' },
+  { path: 'http_client', name: 'HTTP CLIENT' },
+  { path: 'http_collect', name: 'HTTP COLLECT' },
+  { path: 'juggling_async', name: 'JUGGLING ASYNC' },
+  { path: 'time_server', name: 'TIME SERVER' },
+  { path: 'http_file_server', name: 'HTTP FILE SERVER' },
+  { path: 'http_uppercaserer', name: 'HTTP UPPERCASERER' },
+  { path: 'http_json_api_server', name: 'HTTP JSON API SERVER' }
 ]
 
-console.log(titles)
+async function writeMd (m, index) {
+  const num = ++index
+  const lnum = num < 10 ? `0${num}` : num
+  const title = `${m.name} (Exercise ${num} of ${meta.length})`
 
-const ex = '/usr/local/lib/node_modules/learnyounode/exercises/'
-fs.readdir(ex, (error, files) => {
-  files.map((file, index) => {
-    let num = ++index
-    if (num < 10) {
-      num = `0${num}`
-    }
-    console.log(num+file)
-  })
-}) && process.exit()
+  let md = `### ${title}\n`
 
-// TODO: fix this up a bit so it isn't complete trash
-fs.readdir('.', (error, files) => {
-  const folders = files.filter(file => /\d\d-\D/.test(file))
-  folders.map(folder => {
-    const md_file = `${folder}/README.md`
-    fs.readFile(`${folder}/index.js`, (error, js) => {
-      fs.readFile(md_file, (error, md) => {
-        const title = md.toString().split('\n')[0]
-        const _md = `${title}
-\`\`\`js
-${js.toString()}\`\`\`
-`
-        fs.writeFile(md_file, _md, error => {
-          if (error) throw error
-        })
-      })
-    })
-  })
+  const local_folder = `${lnum}-${m.path}`
+  const ex_problem_md = await fs.readFileSync(`${ex}${m.path}/problem.md`)
+  const _ = '----------------------------------------------------------------------'
+  const problem_txt = ex_problem_md.toString().split(_)[0]
+  const files = await fs.readdirSync(local_folder).filter(file => /\.js$/.test(file))
+
+  md += `#### Problem:\n`
+  md += problem_txt
+
+  md += `#### Solution:\n`
+  for (let file of files) {
+    const js = await fs.readFileSync(`${local_folder}/${file}`).toString()
+    md += `${file}\n`
+    md += '```\n'
+    md += js
+    md += '```\n'
+  }
+
+  fs.writeFileSync(`${local_folder}/README.md`, md)
+}
+
+meta.map((m, index) => {
+  writeMd(m, index)
 })
