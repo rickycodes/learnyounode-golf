9cba1ca0a7b3b540c75f2f92e4f806af4a874559
Ricky Miller <ricky.miller@gmail.com>
2018-03-05 20:01:45 -0500

    that -e goodness

--- scripts/test_solutions.sh
+++ scripts/test_solutions.sh
@@ -1,22 +1,23 @@
-learnyounode=./tmp/node_modules/learnyounode
-bin=${learnyounode}/bin/learnyounode
-counter=0
+l=learnyounode
+path=./tmp/node_modules/${l}
+bin=${path}/bin/${l}
+count=0
 
-if [ ! -d "$learnyounode" ]; then
-  echo learnyounode directory does not exist!
+if [ ! -d "$path" ]; then
+  echo ${l} directory does not exist!
   exit 1
 fi
 
 for folder in $(ls -d */ | grep '[0-13]');
 do
-  ${bin} select ${counter} &> /dev/null
+  ${bin} select ${count} &> /dev/null
   verify=$(${bin} verify ${folder}/index.js)
-  echo ${verify}
+  echo -e "${verify}\n"
   check=$(echo ${verify} | grep FAIL)
   if [ "$check" ]; then
     exit 1
   fi
-  let counter=counter+1
+  let count=count+1
 done
 
-echo all tests passed!
+echo -e "\nall tests passed!"
